ef68541609a4f5954a07bd31fd37ebd90956542f
atilgner <atilgner@digital-strata.com>
2023-01-28 01:00:02 -0800

    started adding types

---
+++ src/index.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) AXA Group Operations Spain S.A.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { Language } from "./language";
+import { NlpUtil, NlpManager, NlpExcelReader } from "./nlp";
+import { XTableUtils, XTable, XDoc } from "./xtables";
+import { removeEmojis, Evaluator, SpellCheck, Handlebars } from "./util";
+import { ActionManager, NlgManager } from "./nlg";
+import { NeuralNetwork } from "./classifiers";
+import { SentimentAnalyzer, SentimentManager } from "./sentiment";
+import {
+  Recognizer,
+  ConversationContext,
+  MemoryConversationContext,
+} from "./recognizer";
+import { BrainNLU } from "./nlu";
+
+export default {
+  Language,
+  NlpUtil,
+  NlpManager,
+  NlpExcelReader,
+  XTableUtils,
+  XTable,
+  XDoc,
+  removeEmojis,
+  Evaluator,
+  SpellCheck,
+  Handlebars,
+  ActionManager,
+  NlgManager,
+  NeuralNetwork,
+  SentimentAnalyzer,
+  SentimentManager,
+  Recognizer,
+  ConversationContext,
+  MemoryConversationContext,
+  BrainNLU,
+};
